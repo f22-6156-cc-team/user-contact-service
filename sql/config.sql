f72e00188ea92233cf0ab62e6e511e0ce2257990
Weicheng Zhao <zhaoweicheng98@gmail.com>
2022-12-02 17:45:23 -0500

    Update sql config

--- sql/config.sql
+++ sql/config.sql
@@ -6,9 +6,9 @@ create table user_contacts.UserContacts
 (
     user_id            varchar(255) not null,
     is_active          boolean      not null,
-    primary_email_id   int          null,
-    primary_phone_id   int          null,
-    primary_address_id int          null,
+    primary_email_id   varchar(255) null,
+    primary_phone_id   varchar(255) null,
+    primary_address_id varchar(255) null,
     constraint UserContacts_pk
         primary key (user_id)
 );
@@ -17,12 +17,12 @@ drop table if exists user_contacts.email;
 create table user_contacts.email
 (
     user_id       varchar(255)                        not null,
-    email_id      int                                 not null auto_increment,
+    email_id      varchar(255)                        not null,
     is_active     boolean                             not null,
     email_type    enum ('Personal', 'School', 'Work') not null,
     email_address varchar(255)                        not null,
     constraint email_pk
-        primary key (email_id),
+        primary key (user_id, email_id),
     constraint email_usercontacts_null_fk
         foreign key (user_id) references user_contacts.UserContacts (user_id)
 );
@@ -31,11 +31,11 @@ drop table if exists user_contacts.phone;
 create table user_contacts.phone
 (
     user_id      varchar(255)                    not null,
-    phone_id     int                             not null auto_increment,
+    phone_id     varchar(255)                    not null,
     is_active    boolean                         not null,
     phone_type   enum ('Home', 'Work', 'Mobile') not null,
     phone_number varchar(31)                     not null,
-    primary key (phone_id),
+    primary key (user_id, phone_id),
     constraint phone_usercontacts_null_fk
         foreign key (user_id) references user_contacts.UserContacts (user_id)
 );
@@ -44,7 +44,7 @@ drop table if exists user_contacts.address;
 create table user_contacts.address
 (
     user_id       varchar(255)                                 not null,
-    address_id    int                                          not null auto_increment,
+    address_id    varchar(255)                                 not null,
     is_active     boolean                                      not null,
     address_type  enum ('Permanent', 'Temporary', 'Mail-only') not null,
     address_line1 varchar(255)                                 not null,
@@ -52,7 +52,7 @@ create table user_contacts.address
     city          varchar(255)                                 not null,
     state         varchar(63)                                  not null,
     zip           varchar(15)                                  not null,
-    primary key (address_id),
+    primary key (user_id, address_id),
     constraint address_usercontacts_null_fk
         foreign key (user_id) references user_contacts.UserContacts (user_id)
 );
